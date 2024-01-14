ae42ae4277ea2269480fa3b21ed2fe08c08ee118
Sergio Vergara <sergiovergara123@gmail.com>
2024-01-13 21:06:27 -0500

    ex2 completed

--- 02-010724-profitable-gamble.js
+++ 02-010724-profitable-gamble.js
@@ -1,6 +1,6 @@
 // Src: https://edabit.com/challenge/ghbHrRnRiDz9fvQNF
 
-// Profitable Gamble
+// Profitable Gamblegit 
 // Create a function that takes three arguments prob, prize, pay and returns true if prob * prize > pay; otherwise return false.
 
 // To illustrate:
@@ -18,13 +18,17 @@
 // A profitable gamble is a game that yields a positive net profit, where net profit is calculated in the following manner: net_outcome = probability_of_winning * prize - cost_of_playing.
 
 function profitableGamble(prob, prize, pay) {
-	
+	if (prob * prize > pay) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-Test.assertEquals(profitableGamble(0.2, 50, 9), true)
-Test.assertEquals(profitableGamble(0.9, 1, 2), false)
-Test.assertEquals(profitableGamble(0.9, 3, 2), true)
-Test.assertEquals(profitableGamble(0.33, 10, 3.30), true)
-Test.assertEquals(profitableGamble(0, 1000, 0.01), false)
-Test.assertEquals(profitableGamble(0.1, 1000, 7), true)
-Test.assertEquals(profitableGamble(0, 0, 0), false)
+console.log(profitableGamble(0.2, 50, 9)) // true
+console.log(profitableGamble(0.9, 1, 2) )// false
+console.log(profitableGamble(0.9, 3, 2)) // true
+console.log(profitableGamble(0.33, 10, 3.30)) // true
+console.log(profitableGamble(0, 1000, 0.01) )// false
+console.log(profitableGamble(0.1, 1000, 7)) // true
+console.log(profitableGamble(0, 0, 0) )// false
